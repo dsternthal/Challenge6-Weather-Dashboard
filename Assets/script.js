934be6f8819ffe7ff5b1214c070433363057f997
Dani Sternthal <dstrnthl97@gmail.com>
2023-09-10 22:22:01 -0400

    save work

--- Assets/script.js
+++ Assets/script.js
@@ -5,6 +5,7 @@ var windEl = document.getElementById("wind")
 var humidityEl = document.getElementById("humidity")
 var searchBtn = document.getElementById("search-btn")
 var cityInput = document.getElementById("city-input")
+var fivedayForecastEl=document.getElementById("fiveday-forecast")
 
 
 
@@ -23,7 +24,7 @@ var cityInput = document.getElementById("city-input")
     })
     .then(function(currentData){
         console.log(currentData)
-        titleEl.innerHTML=currentData.name+dayjs.unix(currentData.dt).format("MM/DD/YYYY")
+        titleEl.innerHTML=currentData.name + dayjs.unix(currentData.dt).format(" (MM/DD/YYYY)") + "<img src'https://openweathermap.org/img/wn/"+currentData.weather[0].icon+"@2x.png'>"
     })
 
     var forecastUrl="https://api.openweathermap.org/data/2.5/forecast?q="+cityName+"&appid="+apiKey+"&units=imperial"
@@ -36,8 +37,10 @@ var cityInput = document.getElementById("city-input")
         console.log(forecastData)
         //grab 12:00PM each day for 5 days
         var forecastArr=forecastData.list
-        for (let i = 3; i < forecastArr.length; i=i+8) {
+        fivedayForecastEl.textContent=""
+        for (let i = 3, j=1; i < forecastArr.length; i=i+8, j++) {
             console.log(forecastArr[i])
+            var cardTitle=document.getElementById("card-title")
         }
     })
   }
